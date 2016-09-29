a68c2826d900f3e871b14c66296c4ce8d266511c
matthewgoshman <matthew.goshman@gmail.com>
2016-09-28 17:45:44 -0700

    Silently ignore Kara Place's face when she wears glasses

--- site/build/main.js
+++ site/build/main.js
@@ -7042,6 +7042,18 @@
 	      if (!json) {
 	        return;
 	      }
+	
+	      // Handle Kara Place Face Condition
+	      if (!json.responses) {
+	        window.console.log('Kara Place Identified');
+	        return;
+	      }
+	      if (!json.responses[0].faceAnnotations) {
+	        window.console.log('Kara Place Identified');
+	        return;
+	      }
+	      //
+	
 	      this.canvasUtils.loadImage(json, imgPath);
 	    }
 	  }, {
